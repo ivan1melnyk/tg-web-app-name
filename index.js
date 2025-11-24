489a6bf6561ff4a7527aae599d00c05ebee66629
ivan1melnyk <139176450+ivan1melnyk@users.noreply.github.com>
2025-11-24 16:31:45 +0100

    initilace

---
+++ index.js
@@ -0,0 +1,33 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+require("dotenv").config(); // Add this at the top of your file
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const webAppUrl = "https://www.google.com/";
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text == "/start") {
+    await bot.sendMessage(
+      chatId,
+      "A button will appear below, fill out the form",
+      {
+        reply_markup: {
+          keyboard: [[{ text: "Fill out the form" }]],
+        },
+      }
+    );
+
+    await bot.sendMessage(chatId, "Go to our online store", {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Make order", web_app: { url: webAppUrl } }],
+        ],
+      },
+    });
+  }
+
+  await bot.sendMessage(chatId, "Received your message");
+});
