b9a16174875fcf9156e564b0abfcb0efedda48bc
ivan1melnyk <139176450+ivan1melnyk@users.noreply.github.com>
2025-11-29 11:19:57 +0100

    advensted form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,21 +4,31 @@ import { useTelegram } from '../../hooks/useTelegram';
 
 
 const Form = () => {
+    const [first_name, setFirstName] = useState('');
+    const [last_name, setLastName] = useState('');
+    const [phone_number, setPhoneNumber] = useState('');
+    
     const [country, setCountry] = useState('');
     const [city, setCity] = useState('');
     const [street, setStreet] = useState('');
+    const [house_number, setHouseNumber] = useState('');
+
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
+            first_name,
+            last_name,
+            phone_number,
             country,
             city,
             street,
+            house_number,
             subject
         }
         tg.sendData(JSON.stringify(data));  
-    }, [country, city, street, subject])
+    }, [first_name, last_name, phone_number, country, city, street, house_number, subject])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
@@ -41,6 +51,18 @@ const Form = () => {
         }
     }, [country, street]);
 
+    const onChangeFirstName = (e) => {
+        setFirstName(e.target.value);
+    }  
+
+    const onChangeLastName = (e) => {
+        setLastName(e.target.value);
+    }
+
+    const onChangePhoneNumber = (e) => {
+        setPhoneNumber(e.target.value);
+    }
+
     const onChangeCountry = (e) => {
         setCountry(e.target.value);
     }
@@ -53,16 +75,26 @@ const Form = () => {
         setStreet(e.target.value);
     }
 
+    const onChangeHouseNumber = (e) => {
+        setHouseNumber(e.target.value);
+    }
+
     const onChangeSubject = (e) => {
         setSubject(e.target.value);
     }
 
     return (
         <div className="form">
-            <h3>Please enter data</h3>
+            <h2> Regisstration Form</h2>
+            <h3>Please enter receiver info</h3>
+            <input type="text" placeholder={'first name'} value={first_name} onChange={onChangeFirstName}/>
+            <input type="text" placeholder={'last name'} value={last_name} onChange={onChangeLastName}/>
+            <input type="text" placeholder={'phone number'} value={phone_number} onChange={onChangePhoneNumber}/>
+            <h3>Please enter your adress info</h3>
             <input type="text" placeholder={'Country'} value={country} onChange={onChangeCountry}/>
             <input type="text" placeholder={'City'} value={city} onChange={onChangeCity}/>
             <input type="text" placeholder={'Street'} value={street} onChange={onChangeStreet}/>
+            <input type="text" placeholder={'apartment number, floor, etc'} value={house_number} onChange={onChangeHouseNumber}/>
             <select value={subject} onChange={onChangeSubject} className={'select'}>
                 <option value={'physical'}>Natural person</option>
                 <option value={'legal'}>Legal person</option>
